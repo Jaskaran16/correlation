effcb93a52ddbb3c0e9a00a1b6750409413742dd
Jaskaran16 <69311941+Jaskaran16@users.noreply.github.com>
2021-07-02 16:52:11 +0530

    Add files via upload

---
+++ correlation1.py
@@ -0,0 +1,6 @@
+import plotly.express as px
+import csv
+with open("tempandice.csv") as f:
+    df = csv.DictReader(f)
+    fig = px.scatter(df,x = "Temperature", y = "Ice-cream Sales( â‚¹ )")
+    fig.show()    
